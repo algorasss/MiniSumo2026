72eb865850c5ac8f4654547d8164eef3427f286b
algora <timespjump@gmail.com>
2026-02-20 23:12:40 +0200

    messing with multiplexer

--- src/test.cpp
+++ src/test.cpp
@@ -4,15 +4,16 @@
 #include "ClosedCube_TCA9548A.h"
 
 #define TCA9548A_I2C_ADDRESS 0x70
-ClosedCube::Wired::TCA9548A tca(TCA9548A_I2C_ADDRESS); //init the multiplexer right away
+HardwareSerial BTSerial(2);
+ClosedCube::Wired::TCA9548A tca; // init the multiplexer right away
 Adafruit_VL53L0X sensor = Adafruit_VL53L0X();
 
-const uint8_t NUM_SENSORS = 5; // we have 5 sensors
+const uint8_t NUM_SENSORS = 8; // we have 5 sensors
 
-const int motorA1 = PB13;     // пін для правого мотора вперед
-const int motorA2 = PB14;     // пін для правого мотора назад
-const int motorB1 = PA6;      // пін для лівого мотора вперед
-const int motorB2 = PA5;      // пін для лівого мотора назад
+const int motorA1 = PB13; // пін для правого мотора вперед
+const int motorA2 = PB14; // пін для правого мотора назад
+const int motorB1 = PA6;  // пін для лівого мотора вперед
+const int motorB2 = PA5;  // пін для лівого мотора назад
 
 const int lineSensor1 = PA15; //
 const int lineSensor2 = PB3;  //
@@ -25,6 +26,14 @@ bool lastButtonState = false;
 uint16_t distances[NUM_SENSORS]; // start from 1 not 0
 
 int timesRan = 0;
+
+void printAddress(uint8_t address)  {
+	BTSerial.print("0x");
+	if (address<0x10) {
+		BTSerial.print("0");
+	}
+	BTSerial.println(address,HEX);
+}
 
 void StopMotors()
 {
@@ -63,7 +72,6 @@ void TestMotor()
     StopMotors();
     delay(1000);
 
-
     analogWrite(motorB1, 100);
     analogWrite(motorB2, 0);
     delay(1000);
@@ -93,56 +101,96 @@ void TestMotor()
     delay(1000);
 }
 
-
 void TestSensors()
 {
-    for (uint8_t channel = 1; channel < NUM_SENSORS + 1; channel++) // checking sensors on all channels of the multiplexer
+}
+void InitSensorsTemp()
+{
+    tca.address(TCA9548A_I2C_ADDRESS);
+
+    uint8_t returnCode = 0;
+    uint8_t address;
+    uint8_t numberOfDevices;
+
+    for (uint8_t channel = 0; channel < TCA9548A_MAX_CHANNELS; channel++)
     {
+        BTSerial.print("Scanning channel #");
+        BTSerial.print(channel);
+        BTSerial.println("...");
 
-        SerialUSB.print(channel);
+        returnCode = tca.selectChannel(channel);
+        numberOfDevices = 0;
 
-        tca.selectChannel(channel);
-        delay(2);
-        VL53L0X_RangingMeasurementData_t measure;
-        sensor.rangingTest(&measure, true);
-        // if (measure.RangeMilliMeter < 50)
-        // {
-            SerialUSB.print(" range: ");
-            SerialUSB.print(measure.RangeMilliMeter);
-            SerialUSB.print(" max range: ");
-            SerialUSB.print(measure.RangeDMaxMilliMeter);
-            SerialUSB.print(" fractional range: ");
-            SerialUSB.print(measure.RangeFractionalPart);
-            SerialUSB.print(" time stamp: ");
-            SerialUSB.println(measure.TimeStamp);
-        //}
+        if (returnCode == 0)
+        {
+            // for (address = 0x01; address < 0x7F; address++)
+            // {
+                Wire.beginTransmission(address);
+                Wire.write(1 << channel);
+                returnCode = Wire.endTransmission();
+                BTSerial.println(returnCode);
+                if (returnCode == 0)
+                {
+                    BTSerial.print("I2C device = ");
+                    printAddress(address);
+
+                    numberOfDevices++;
+                }
+                else if (returnCode == 4)
+                {
+                    BTSerial.print("Unknown error at ");
+                    printAddress(address);
+                }
+                else
+                {
+                    BTSerial.print(returnCode);
+                    BTSerial.println("Fail");
+                }
+                if (numberOfDevices == 0)
+                    BTSerial.println("No I2C devices found\n");
+                else
+                {
+                    BTSerial.print("Error scan channel (Code:");
+                    BTSerial.print(returnCode);
+                    BTSerial.println(")");
+                }
+            //}
+            BTSerial.println("where am i");
+        } else
+        {
+            BTSerial.print("Error scan channel (Code:");
+            BTSerial.print(returnCode);
+            BTSerial.println(")");
+        }
     }
 }
 void InitSensors()
 {
-    for (uint8_t channel = 1; channel < NUM_SENSORS + 1; channel++) // checking sensors on all channels of the multiplexer
+    uint8_t returnCode = 0;
+    uint8_t numberOfDevices;
+
+    for (uint8_t channel = 0; channel < NUM_SENSORS; channel++)
     {
-        SerialUSB.print("SETUP sensor on channel ");
-        SerialUSB.print(channel);
-        SerialUSB.println("... ");
-        tca.selectChannel(channel);
-        delay(2);
-        if (sensor.begin())
+        BTSerial.print("Scanning channel #");
+        BTSerial.print(channel);
+        BTSerial.println("...");
+
+        for(uint8_t address = 0x01; address < 0x7F; address++)
         {
-            SerialUSB.println("s OK");
-        }
-        else
-        {
-            SerialUSB.println("s FAILED");
+            BTSerial.print("Trying address: ");
+            BTSerial.println(address, HEX);
+            Wire.beginTransmission(address);
+            Wire.write(1 << channel);
+            returnCode = Wire.endTransmission();
+            BTSerial.println(returnCode);
         }
     }
 }
 
-
 void TestLineSensors(bool isFirst)
 {
-    //maybe after detecting a white line, delay cuz its tweaking
-    if(isFirst)
+    // maybe after detecting a white line, delay cuz its tweaking
+    if (isFirst)
     {
         // for(int i = 0; i < 5; i++)
         // {
@@ -154,7 +202,7 @@ void TestLineSensors(bool isFirst)
         int sensor1 = digitalRead(lineSensor1);
         SerialUSB.print("sensor 1 value: ");
         SerialUSB.println(sensor1);
-        if(sensor1 == 1)
+        if (sensor1 == 1)
         {
             SerialUSB.println("WWWW");
         }
@@ -164,12 +212,14 @@ void TestLineSensors(bool isFirst)
         int sensor2 = digitalRead(lineSensor2);
         SerialUSB.print("sensor 2 value: ");
         SerialUSB.println(sensor2);
-        if(sensor2 == 1)
+        if (sensor2 == 1)
         {
             SerialUSB.println("WWWW");
         }
     }
 }
+
+
 
 void setup()
 {
@@ -183,40 +233,42 @@ void setup()
 
     pinMode(buttonPin, INPUT_PULLUP);
 
-    SerialUSB.begin(115200);
-    SerialUSB.println("setting up...");
+    BTSerial.begin(9600);
+    BTSerial.println("setting up...");
 
-    Wire.begin();
-    Wire.setClock(400000);
+    Wire.begin(PB7, PB6); // SDA, SCL
+    Wire.setClock(100000);
 
     InitSensors();
 }
 void loop()
 {
-    buttonState = digitalRead(buttonPin);
-    if (buttonState == LOW && lastButtonState == HIGH)
-    {
-        delay(50);
-        SerialUSB.println("Re-initializing sensors...");
-        Wire.end();
-        delay(100);
-        Wire.begin();
-        Wire.setClock(400000);
-        InitSensors();
-        //delay(1000);
-    }
-    lastButtonState = buttonState;
-    //test motors code
-    // if(timesRan < 5)
+    //BTSerial.println("looping...");
+    //delay(100);
+    // buttonState = digitalRead(buttonPin);
+    // if (buttonState == LOW && lastButtonState == HIGH)
     // {
-    //     timesRan++;
-    //     TestMotor();
+    //     delay(50);
+    //     SerialUSB.println("Re-initializing sensors...");
+    //     Wire.end();
+    //     delay(100);
+    //     Wire.begin();
+    //     Wire.setClock(400000);
+    //     InitSensors();
+    //     // delay(1000);
     // }
+    // lastButtonState = buttonState;
+    // test motors code
+    //  if(timesRan < 5)
+    //  {
+    //      timesRan++;
+    //      TestMotor();
+    //  }
 
-    //test line sensors
-    //TestLineSensors(false); //line sensor
+    // test line sensors
+    // TestLineSensors(false); //line sensor
 
-    //test enemy sensors
-    TestSensors();
+    // test enemy sensors
+    //TestSensors();
 }
-//make program start on button click, if button is clicked while code is running, switch to the next mode, if it is held for 2 second, stop
+// make program start on button click, if button is clicked while code is running, switch to the next mode, if it is held for 2 second, stop
